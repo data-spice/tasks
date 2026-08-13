7db2bca7a7e609eb334a2c703097546e0e5ca968
data-spice <myvicgit@gmail.com>
2026-08-13 12:03:27 +0300

    Added a function with multiple parameters

--- lesson2/functions.py
+++ lesson2/functions.py
@@ -8,9 +8,17 @@
 
 ####    FUNCTIONS WITH A PARAMETER
 
-def greet_any(name):
-    print(f"Hello {name}")
+# def greet_any(name):
+#     print(f"Hello {name}")
 
 
-greet_any("Peris")
-greet_any("Victor")
+# greet_any("Peris")
+# greet_any("Victor")
+
+####    FUNC WITH MULTIPLE PARAMETERS
+
+def id(name,phone_no):
+    print(f"Name: {name}  Phone Number: {phone_no}")
+
+id("David",24555262728)
+id("Kush ", 12345)
